6451b97b12b41e98eff4b4ff21e9f9dcc8ed5b96
Kawser-WD <kawser.wd@gmail.com>
2025-03-05 12:50:11 +0600

    update video issue solved

--- src/components/supermarket/CourseCart.jsx
+++ src/components/supermarket/CourseCart.jsx
@@ -1,32 +1,26 @@
 const CourseCart = ({ video }) => {
   return (
     <div className="w-full px-4">
-      <h1 className="text-center py-6 text-2xl font-bold">
+      <h1 className="text-center py-6 text-2xl font-bold" dir="rtl">
         {video?.videoTitle}
       </h1>
       <div className="w-full flex justify-center">
         <div className="w-full md:w-3/4 lg:w-2/3">
-          <iframe
-            src={video?.videoLink}
-            // src={
-            //   "https://res.cloudinary.com/dt3qrt76n/video/upload/v1716032300/Videos/Course_Status_Programming_Hero_9_ivnnxe.mp4"
-            // }
-            width="100%"
-            height="500px"
-            allow="autoplay"
-            className="aspect-video overflow-hidden object-cover"
-          ></iframe>
-
-          {/* <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/iajv8y-6n3Q?si=BR-YRbRtp3r8r_hG"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-          ></iframe> */}
+          <div
+            className="voomly-embed"
+            data-id={video?.videoLink}
+            data-ratio="0.562500"
+            data-type="v"
+            data-skin-color="rgba(209,43,40,1)"
+            data-shadow=""
+            style={{
+              width: "100%",
+              aspectRatio: "0.5625 / 1",
+              background:
+                "linear-gradient(45deg, rgb(142, 150, 164) 0%, rgb(201, 208, 222) 100%)",
+              borderRadius: "10px",
+            }}
+          ></div>
         </div>
       </div>
     </div>
