eef9986145b3f23758c500411fc075f1a4fc4c4b
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2025-03-17 23:42:51 +0600

    Metric tracking

--- src/components/measurements/measurementWatch/SingleCart.jsx
+++ src/components/measurements/measurementWatch/SingleCart.jsx
@@ -1,6 +1,9 @@
 import { mwatchData } from "@/constants/mwatchData";
 import { pixelCartImg } from "@/assets";
 import SmallCart from "./SmallCart";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { base_url } from "@/api/baseUrl";
 
 const SingleCart = () => {
   const getButtonClass = (data) => {
@@ -14,12 +17,32 @@ const SingleCart = () => {
     return "bg-blue-500 hover:bg-blue-600 text-white text-xs font-bold";
   };
 
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${base_url}/measurement/calculate/67d6f2fe2899b65e6f5951bf`);
+        //const data = await response.json();
+        console.log("data",response);
+        
+        setData(response?.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData()
+
+  }, []);
+  console.log("data",data);
+  
+
   return (
     <div
       className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pb-20 pt-10 p-4"
       dir="rtl"
     >
-      {mwatchData.map((data) => (
+      {data.map((data) => (
         <div
           key={data._id}
           dir="rtl"
@@ -32,19 +55,19 @@ const SingleCart = () => {
         >
           {/* Title and Icon */}
           <div className="flex gap-2 items-center space-x-2 text-white">
-            <img src={data.icon} alt="icon" className="w-6 h-6 text-white" />
+            {/* <img src={data.icon} alt="icon" className="w-6 h-6 text-white" /> */}
             <h3 className="text-lg font-bold">{data.cartTitle}</h3>
           </div>
 
           <div className="flex justify-center items-start">
             {" "}
-            <button
+            {/* <button
               className={`py-2 md:w-[70%] w-[90%] rounded-full text-center ${getButtonClass(
                 data
               )}`}
             >
               {data.button}
-            </button>
+            </button> */}
           </div>
           {/* Items */}
 
