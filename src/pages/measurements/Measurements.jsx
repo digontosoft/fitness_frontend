a54ede3544729594fed3c1502c2cdf5e4b3c424d
abircu <mdabircse4873@gmail.com>
2025-03-17 14:21:58 +0600

    mesurement form updated

--- src/pages/measurements/Measurements.jsx
+++ src/pages/measurements/Measurements.jsx
@@ -1,15 +1,19 @@
 import VideoCart from "@/components/common/VideoCart";
 import InputForm from "@/components/measurements/InputForm";
 import Title from "@/components/measurements/Tilte";
+import HeroVideo from "@/components/startTraining/HeroVideo";
 
 import React from "react";
 
 const Measurements = () => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
   return (
     <div>
-      <VideoCart
-        sourceLink={
-          "https://www.youtube.com/embed/iKJWC6dDjKQ?si=7a-XGnGoitNcdyJg"
+      <HeroVideo
+        videoUrl={
+          userInfo?.gender === "male"
+            ? "https://youtu.be/iKJWC6dDjKQ?feature=shared"
+            : "https://www.youtube.com/watch?v=uUo9Bw5ytrI"
         }
       />
       <Title tilte={"הזנת מדדים"} />
