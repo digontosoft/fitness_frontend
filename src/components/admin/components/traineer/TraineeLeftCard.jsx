6e26143294f143c7317aad1006956ba214cb2e96
abircu <mdabircse4873@gmail.com>
2025-03-19 12:21:36 +0600

    resoved build issue

--- src/components/admin/components/traineer/TraineeLeftCard.jsx
+++ src/components/admin/components/traineer/TraineeLeftCard.jsx
@@ -1,20 +1,3 @@
-// import { Button } from "@/components/ui/button";
-// import { image } from "@/assets/index";
-// import { Link } from "react-router-dom";
-
-// const TraineeLeftCard = ({ userId }) => {
-//   return (
-//     <div
-//       className="w-72 h-48 bg-[#0A0A0A] p-2 rounded-2xl "
-//       style={{ backgroundImage: `url(${image})` }}
-//     >
-//       <div className="flex justify-between items-start" dir="ltr"></div>
-//     </div>
-//   );
-// };
-
-// export default TraineeLeftCard;
-
 import { Button } from "@/components/ui/button";
 import {
   icon,
