f145529cd8ca1864584c06e97efd884a7bb06517
Kawser-WD <kawser.wd@gmail.com>
2025-03-19 16:17:21 +0600

    update

--- src/pages/admin/assigntraining/AssignTraining.jsx
+++ src/pages/admin/assigntraining/AssignTraining.jsx
@@ -4,7 +4,6 @@ import FormTitle from "@/components/admin/components/ui/FormTitle";
 
 const AssignTraining = () => {
   const { userId } = useParams();
-  console.log(userId);
   return (
     <div className="bg-customBg relative flex items-center justify-center min-h-screen mb-2">
       <div className="bg-white shadow-lg rounded-[60px] p-6 w-5/6 min-h-[80vh] h-auto flex flex-col items-center justify-center my-4">
