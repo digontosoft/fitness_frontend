bf08f3869f3bf0b1278dfc3ecc6db177ca6136f7
Kawser-WD <kawser.wd@gmail.com>
2025-05-22 16:56:09 +0600

    update

--- src/pages/measurements/MeasurementsWatch.jsx
+++ src/pages/measurements/MeasurementsWatch.jsx
@@ -10,8 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import axios from "axios";
 import React, { useEffect } from "react";
-import { use } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const MeasurementsWatch = () => {
   const location = useLocation();
@@ -21,9 +20,7 @@ const MeasurementsWatch = () => {
   const userId = queryParams.get("userId");
   const [open, setOpen] = React.useState(false);
   const [id, setId] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
-  const [data, setData] = React.useState([]);
-  console.log("id:", id);
+  const [measurements, setMeasurements] = React.useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,8 +28,7 @@ const MeasurementsWatch = () => {
         const response = await axios.get(
           `${base_url}/get-measurement-by-id/${id}`
         );
-        console.log("response:", response);
-        setData(response?.data?.data);
+        setMeasurements(response?.data?.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -40,7 +36,7 @@ const MeasurementsWatch = () => {
     fetchData();
   }, [id]);
 
-  console.log("data:", data);
+  console.log("measurements:", measurements);
 
   return (
     <div>
@@ -49,12 +45,14 @@ const MeasurementsWatch = () => {
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
           <div className="min-h-44 px-6" dir="rtl">
-            {!data.photo1 && <span dir="rtl">לא הוספת תמונות למדידה״</span>}
-            {data.photo1 && (
+            {!measurements.photo1 && (
+              <span dir="rtl">לא הוספת תמונות למדידה״</span>
+            )}
+            {measurements.photo1 && (
               <div className="grid justify-center gap-3">
-                <img src={`${base_url}/${data.photo1}`} alt="" />
-                <img src={`${base_url}/${data.photo2}`} alt="" />
-                <img src={`${base_url}/${data.photo3}`} alt="" />
+                <img src={`${base_url}/${measurements.photo1}`} alt="" />
+                <img src={`${base_url}/${measurements.photo2}`} alt="" />
+                <img src={`${base_url}/${measurements.photo3}`} alt="" />
               </div>
             )}
           </div>
