58675ced943cd0323ed55bc507450e710e0eb335
Kawser-WD <kawser.wd@gmail.com>
2025-05-22 16:30:11 +0600

    update

--- src/components/common/VideoCourseCart.jsx
+++ src/components/common/VideoCourseCart.jsx
@@ -1,34 +1,3 @@
-// import { FaArrowLeftLong } from "react-icons/fa6";
-// import { Button } from "../ui/button";
-// import ReactPlayer from "react-player";
-
-// const VideoCourseCart = ({ exercise }) => {
-//   return (
-//     <div className="w-full md:w-96 flex gap-2 items-center justify-between px-2 py-2 bg-[#FBFBFB] rounded-2xl shadow-md shadow-gray-300">
-//       <Button className="rounded-2xl">
-//         <FaArrowLeftLong />
-//       </Button>
-//       <div className="w-full flex  items-center justify-center">
-//         <h1 className="text-sm font-bold text-[#0A2533]">
-//           {" "}
-//           {exercise?.exercise_id?.name}
-//         </h1>
-//       </div>
-//       <div className="w-full max-w-xs mx-auto">
-//         <ReactPlayer
-//           url={exercise?.exercise_id?.video_url}
-//           width="100%"
-//           height="80px"
-//           controls
-//           className="rounded-lg border border-gray-200 shadow-sm"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoCourseCart;
-
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { Button } from "../ui/button";
 import ReactPlayer from "react-player";
@@ -40,7 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "../ui/dialog"; // Adjust path if needed
+} from "../ui/dialog";
 
 const VideoCourseCart = ({ exercise }) => {
   return (
