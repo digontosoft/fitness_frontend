6b84170fe88a6ed7d2ef255a83b3606cb0262c95
abircu <mdabircse4873@gmail.com>
2026-03-09 17:08:56 +0600

    solve all feedback

--- src/components/common/VideoCourseCart.jsx
+++ src/components/common/VideoCourseCart.jsx
@@ -61,53 +61,6 @@ const VideoCourseCart = ({ exercise }) => {
   }
   console.log("exercise:", exercise);
   return (
-    // <Dialog>
-    //   <DialogTrigger asChild>
-    //     <div className="w-full cursor-pointer md:w-96 flex gap-2 items-center justify-between px-2 py-2 bg-[#FBFBFB] rounded-2xl shadow-md shadow-gray-300">
-    //       <Button className="rounded-2xl">
-    //         <FaArrowLeftLong />
-    //       </Button>
-    //       <div className="w-full flex items-center justify-center">
-    //         <h1 className="text-sm font-bold text-[#0A2533]">
-    //           {exercise?.exercise_id?.name}
-    //         </h1>
-    //       </div>
-    //       <div className="w-full max-w-xs mx-auto">
-    //         <ReactPlayer
-    //           url={exercise?.exercise_id?.video_url}
-    //           width="100%"
-    //           height="80px"
-    //           controls
-    //           className="rounded-lg border border-gray-200 shadow-sm"
-    //         />
-    //       </div>
-    //     </div>
-    //   </DialogTrigger>
-
-    //   <DialogContent className="sm:max-w-[720px]">
-    //     <DialogHeader>
-    //       <DialogTitle className="text-center">
-    //         {exercise?.exercise_id?.name}
-    //       </DialogTitle>
-    //     </DialogHeader>
-    //     <div className="w-full">
-    //       <ReactPlayer
-    //         url={exercise?.exercise_id?.video_url}
-    //         width="100%"
-    //         height="360px"
-    //         controls
-    //         className="rounded-lg border border-gray-200 shadow-sm"
-    //       />
-    //     </div>
-    //     <DialogFooter>
-    //       <DialogClose asChild>
-    //         <Button variant="default" className="bg-[#7994CB]">
-    //           Close
-    //         </Button>
-    //       </DialogClose>
-    //     </DialogFooter>
-    //   </DialogContent>
-    // </Dialog>
     <Dialog>
       <DialogTrigger asChild>
         <div className="w-full cursor-pointer md:w-96 flex gap-2 items-center justify-between px-2 py-2 bg-[#FBFBFB] rounded-2xl shadow-md shadow-gray-300">
