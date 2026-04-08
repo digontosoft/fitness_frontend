951c22ae830578ac59ca560a959a55617e4beace
Kawser-WD <kawser.wd@gmail.com>
2026-04-08 21:17:24 +0600

    report download issue solved

--- src/components/admin/components/traineer/TraineerUi.jsx
+++ src/components/admin/components/traineer/TraineerUi.jsx
@@ -10,8 +10,10 @@ import {
   ArrowBurger as NutritionImage,
   ArrowDumbel as TrainingImage,
 } from "@/assets/index";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaArrowLeftLong } from "react-icons/fa6";
 import { toast } from "sonner";
 import AdminArrowCard from "../ui/AdminArrowCard";
 import FormTitle from "../ui/FormTitle";
@@ -206,11 +208,39 @@ const TraineerUi = ({ userId }) => {
           />
         </div>
         <div className="w-[342px]">
-          <AdminArrowCard
+          {/* <AdminArrowCard
             image={trainingImage}
             title="ניהול תפריטי תזונה אישיים"
             link={exerciseReport ? exerciseReport : "#"}
-          />
+          /> */}
+          <div
+        className="w-full h-[100px] flex gap-4 items-center justify-between px-4 py-2 bg-white border border-[#efefef] rounded-2xl shadow-lg cursor-pointer"
+        dir="ltr"
+      >
+        <a href={exerciseReport ? exerciseReport : "#"}>
+        <Button className="rounded-2xl w-[25px] h-6 flex-shrink-0">
+          <FaArrowLeftLong />
+        </Button>
+        </a>
+        <div className="flex items-center gap-4 flex-1 justify-between">
+          <div className="flex-1">
+            <a
+              className="text-sm sm:text-base font-bold leading-5 text-[#0A2533] text-right line-clamp-2"
+              dir="rtl"
+              href={exerciseReport ? exerciseReport : "#"}
+            >
+              ניהול תפריטי תזונה אישיים
+            </a>
+          </div>
+          <div className="w-[95px] h-[90px] flex-shrink-0 overflow-hidden flex items-center justify-center bg-[#F7FAFC] rounded-lg">
+            <img
+              src={trainingImage}
+              alt=""
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+        </div>
+      </div>
         </div>
       </div>
       {/* {openModal && (
